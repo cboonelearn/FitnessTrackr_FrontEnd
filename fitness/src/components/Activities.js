f3e8013f9487e11cf6bba50d7474aab8c8f458bc
Malkahiro <tahiro9011@gmail.com>
2022-08-30 20:27:57 -0500

    Activites Component Started

---
+++ fitness/src/components/Activities.js
@@ -0,0 +1,29 @@
+import { getActivities } from "../api/api";
+import React, { useEffect, useState } from "react";
+
+const Activites = () => {
+    const [activites, setActivites] = useState([]);
+    const handleActivites = () =>{
+        getActivities()
+        .then(results => {
+            setActivites(results)
+            console.log(results)
+        });
+    }
+    useEffect(() =>{
+        handleActivites();
+    }, []);
+    
+    return ( 
+        <div className="Activites">
+            <h1>Activites</h1>
+            {activites.map((activity) =>{ return (<div key={activity.id}>
+                <h2>{activity.name}</h2>
+                <p>{activity.description}</p>
+                </div>)
+            })}
+        </div>
+     );
+}
+
+export default Activites;
---
+++ fitness/src/components/Home.js
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+    
+    return ( 
+        <div className="Home">
+            <h1>Welcome to your Homepage</h1>
+        </div>
+     );
+}
+ 
+export default Home;
